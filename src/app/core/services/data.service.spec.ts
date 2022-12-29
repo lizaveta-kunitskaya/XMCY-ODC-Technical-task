5ac14121f14f2dda1734646caba1702fae2b370b
lizaveta-kunitskaya <lizaveta.kunitskaya@gmail.com>
2022-12-29 21:21:18 +0100

    feat: implement Data service

--- src/app/core/services/data.service.spec.ts
+++ src/app/core/services/data.service.spec.ts
@@ -1,16 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { of, throwError } from 'rxjs';
+
+import { Photo } from '../models/photo.models';
 import { DataService } from './data.service';
 
-describe('DataService', () => {
+const mockData: Photo[] = [{
+  id: 'fakeId',
+}];
+
+fdescribe('DataService', () => {
   let service: DataService;
 
+  const fakeHttpClient = jasmine.createSpyObj('HttpClient', ['get']);
+  fakeHttpClient.get.and.returnValue(of(mockData));
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: HttpClient, useValue: fakeHttpClient },
+      ]
+    });
     service = TestBed.inject(DataService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return data', () => {
+    service.getPhotos(0).subscribe(value => {
+      expect(value).toEqual(mockData);
+    })
+  });
+
+  it('should return an empty array if an error occurs', () => {
+    fakeHttpClient.get.and.returnValue(throwError({ status: 500 }));
+    service.getPhotos(0).subscribe(value => {
+      expect(value).toEqual([]);
+    })
   });
 });
--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Photo } from '../models/photo.models';
+import { BASE_URL } from '../constants/url.constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  constructor(private readonly http: HttpClient) { }
 
-  constructor() { }
+  getPhotos(pageNumber: number): Observable<Photo[]> {
+    return this.http.get<Photo[]>(`${BASE_URL}/v2/list?page=${pageNumber}&limit=20`).pipe(
+      catchError(error => {
+        console.error(error.message);
+
+        return of([]);
+      })
+    );
+  }
 }
