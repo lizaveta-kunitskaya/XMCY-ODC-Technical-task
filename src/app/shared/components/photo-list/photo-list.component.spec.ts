f1f1804c9f613e2b31c06bf758f71f986472ea2c
lizaveta-kunitskaya <lizaveta.kunitskaya@gmail.com>
2022-12-29 21:52:07 +0100

    feat: implement Photo List component

--- src/app/shared/components/photo-list/photo-list.component.spec.ts
+++ src/app/shared/components/photo-list/photo-list.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PhotoListComponent } from './photo-list.component';
 
@@ -6,11 +8,17 @@ describe('PhotoListComponent', () => {
   let component: PhotoListComponent;
   let fixture: ComponentFixture<PhotoListComponent>;
 
+  const fakeRouter = jasmine.createSpyObj('Router', ['navigate']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PhotoListComponent ]
+      declarations: [PhotoListComponent],
+      imports: [RouterTestingModule],
+      providers: [
+        { provide: Router, useValue: fakeRouter },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +29,11 @@ describe('PhotoListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to the Single Photo page', () => {
+    component.goToDetailView('id');
+
+    expect(fakeRouter.navigate).toHaveBeenCalledWith(['photos', 'id']);
   });
 });
--- src/app/shared/components/photo-list/photo-list.component.ts
+++ src/app/shared/components/photo-list/photo-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Photo } from 'src/app/core/models/photo.models';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.scss']
+  styleUrls: ['./photo-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent {
+  @Input() data: Photo[] = [];
+  @Input() photoWidth = 300;
 
-  constructor() { }
+  constructor(private readonly router: Router) { }
 
-  ngOnInit(): void {
+  goToDetailView(id: string) {
+    this.router.navigate(['photos', id]);
   }
-
 }
