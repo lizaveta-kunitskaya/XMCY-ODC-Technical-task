012d40f1e20ca3d4ca849f4b6d89c2e5104d9a63
lizaveta-kunitskaya <lizaveta.kunitskaya@gmail.com>
2022-12-30 12:33:17 +0100

    feat: provide button toggling for Photo component

--- src/app/shared/components/photo/photo.component.spec.ts
+++ src/app/shared/components/photo/photo.component.spec.ts
@@ -1,22 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 
 import { PhotoComponent } from './photo.component';
+import { FavoritePhotoService } from 'src/app/core/services/favorite-photo.service';
 
 describe('PhotoComponent', () => {
   let component: PhotoComponent;
+  let loader: HarnessLoader;
   let fixture: ComponentFixture<PhotoComponent>;
+
+  const fakeFavoritePhotoService = jasmine.createSpyObj('FavoritePhotoService', ['addPhotoToFavorites', 'removePhotoFromFavorites']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PhotoComponent]
+      declarations: [PhotoComponent],
+      providers: [
+        { provide: FavoritePhotoService, useValue: fakeFavoritePhotoService },
+      ],
     })
       .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PhotoComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
     component = fixture.componentInstance;
     component.id = '5';
+    component.isFavorite = false;
     fixture.detectChanges();
   });
 
@@ -32,5 +44,59 @@ describe('PhotoComponent', () => {
     image.dispatchEvent(new Event('click'));
 
     expect(component.imageClicked.emit).toHaveBeenCalledWith('5');
+  });
+
+  it('should add photo to favorites when Like button was clicked', async () => {
+    const likeButtonHarness = MatButtonHarness.with({
+      text: 'Like',
+    });
+    const likeButton = await loader.getHarness(likeButtonHarness);
+
+    await likeButton.click();
+
+    expect(fakeFavoritePhotoService.addPhotoToFavorites).toHaveBeenCalledWith('5');
+  });
+
+  it('should remove photo from favorites when Remove From Favorites button was clicked', async () => {
+    const likeButtonHarness = MatButtonHarness.with({
+      text: 'Like',
+    });
+    const likeButton = await loader.getHarness(likeButtonHarness);
+
+    await likeButton.click();
+
+    const removeFromFavoritesButtonHarness = MatButtonHarness.with({
+      text: 'Remove from favorites',
+    });
+    const removeFromFavoritesButton = await loader.getHarness(removeFromFavoritesButtonHarness);
+
+    await removeFromFavoritesButton.click();
+
+    expect(fakeFavoritePhotoService.removePhotoFromFavorites).toHaveBeenCalledWith('5');
+  });
+
+  it('should toggle action buttons visibility when one of them was clicked', async () => {
+    const likeButtonHarness = MatButtonHarness.with({
+      text: 'Like',
+    });
+    const removeFromFavoritesButtonHarness = MatButtonHarness.with({
+      text: 'Remove from favorites',
+    });
+    const likeButtonVisibility = async () => !!(await loader.getAllHarnesses(likeButtonHarness)).length;
+    const removeFromFavoritesButtonVisibility = async () => !!(await loader.getAllHarnesses(removeFromFavoritesButtonHarness)).length;
+
+    const likeButton = await loader.getHarness(likeButtonHarness);
+
+    await likeButton.click();
+
+    expect(await likeButtonVisibility()).toBeFalse();
+    expect(await removeFromFavoritesButtonVisibility()).toBeTrue();
+
+    const removeFromFavoritesButton = await loader.getHarness(removeFromFavoritesButtonHarness);
+
+    await removeFromFavoritesButton.click();
+
+    expect(await likeButtonVisibility()).toBeTrue();
+    expect(await removeFromFavoritesButtonVisibility()).toBeFalse();
   });
 });
--- src/app/shared/components/photo/photo.component.ts
+++ src/app/shared/components/photo/photo.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 
+import { FavoritePhotoService } from 'src/app/core/services/favorite-photo.service';
+
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html',
@@ -10,14 +12,36 @@ export class PhotoComponent {
   @Input() id?: string;
   @Input() src?: string;
   @Input() alt?: string;
-  @Input() isFavorite?: boolean;
+  @Input() set isFavorite(value: boolean) {
+    this._isFavorite = value;
+  };
   @Input() imageIsClickable?: boolean;
+
+  private _isFavorite?: boolean;
 
   @Output() imageClicked = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(private readonly favoritePhotoService: FavoritePhotoService) { }
+
+  get isFavorite(): boolean {
+    return !!(this._isFavorite);
+  }
 
   imageOnClick() {
     this.imageClicked.emit(this.id);
+  }
+
+  addToFavorites() {
+    if(this.id) {
+      this.favoritePhotoService.addPhotoToFavorites(this.id);
+      this._isFavorite = true;
+    }
+  }
+
+  removeFromFavorites() {
+    if(this.id) {
+      this.favoritePhotoService.removePhotoFromFavorites(this.id);
+      this._isFavorite = false;
+    }
   }
 }
